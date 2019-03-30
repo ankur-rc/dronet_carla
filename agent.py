d7e7dd3109829f3a074397a74c0bf53b45b938d6
Ankur Roy Chowdhury <ankurrc.1993@gmail.com>
2019-03-30 16:37:45 -0500

    Added benchmark runner.

--- agent.py
+++ agent.py
@@ -1,10 +1,12 @@
 from keras.models import (load_model, model_from_json)
 from carla.agent.agent import Agent
 from carla.client import VehicleControl
+from carla.carla_server_pb2 import Measurements
 
 import pathlib
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class DronetAgent(Agent):
@@ -12,12 +14,12 @@ class DronetAgent(Agent):
     def __init__(self, model_directory=None, 
                         architecture_filename="dronet_architecture.json", 
                         weights_filename="dronet_weights.h5",
-                        target_size=(200, 200)):
+                        target_size=(200, 200), alpha=0.7, beta=0.5, max_velocity=10):
         """
         Initialise the class.
         """
 
-        super().__init__()
+        Agent.__init__(self)
         architecture_path = pathlib.Path().joinpath(model_directory, architecture_filename)
         weights_path = pathlib.Path().joinpath(model_directory, weights_filename)
 
@@ -32,23 +34,25 @@ class DronetAgent(Agent):
 
         self.model.load_weights(weights_path)
         self.model.compile(loss="mse", optimizer="sgd")
-        self.target_size = reversed(target_size)
+        self.target_size = (target_size[1], target_size[0])
 
         self.last_theta = 0
-        self.alpha, self.beta = 0.7, 0.5
-        self.velocity_max = 30
+        self.alpha, self.beta = alpha, beta
+        self.velocity_max = max_velocity
 
-    def _process_input(sensor_data):
+    def _process_input(self, sensor_data):
         """
         Preprocess camera image data for use with Dronet.
         """
 
         assert "CameraRGB" in sensor_data, "No 'CameraRGB' found in sensor stream!"
 
-        camera_img = sensor_data['CameraRGB'].data[:,:,:-1][:, :, ::-1]
-        camera_img = cv2.cvtColor(camera_img, cv2.COLOR_BAYER_BG2GRAY)
+        camera_img = sensor_data['CameraRGB'].data[:, :, ::-1]
+        camera_img = cv2.cvtColor(camera_img, cv2.COLOR_BGR2GRAY)
         camera_img = cv2.resize(camera_img, self.target_size)
         camera_img = (camera_img[:, :, np.newaxis]).astype(np.float32)
+
+        
 
         return camera_img
 
@@ -56,21 +60,27 @@ class DronetAgent(Agent):
         """
         Function to run a control step in the CARLA vehicle.
         """
+
         input_img = self._process_input(sensor_data)
-        steer, coll = self.model.predict(input_img[np.newaxis, :, :, :])[0]
+        steer, coll = self.model.predict(input_img[np.newaxis, :, :, :])
+        steer, coll = steer[0, 0], coll[0, 0]
 
         # theta = (1 - beta)*last_theta + beta*pi/2*steer 
-        theta = (1 - self.beta)*self.last_theta + beta*np.pi/2*steer
+        theta = (1 - self.beta)*self.last_theta + self.beta*3.1415/2*steer
 
         # throttle = (1 - alpha)*(current_velocity/max_velocity) + alpha*(1 - coll) if current_velocity < max_velocity else 0
-        velocity = measurements.forward_speed
-        throttle = (1 - self.alpha)*(velocity/self.velocity_max) + self.alpha(1 - coll) if velocity < self.velocity_max else 0.
+        velocity = measurements.player_measurements.forward_speed
+        # throttle = 0.5 if velocity < self.velocity_max else 0.
+        throttle = 0.2 +(1 - self.alpha)*(velocity/self.velocity_max) + self.alpha*(1 - coll) if velocity < self.velocity_max else 0.
 
         self.last_theta = theta
 
         control = VehicleControl()
         control.throttle = throttle
         control.steer = theta
+
+        print("pred-> steer: {}, coll:{}, \t control-> steer: {}, throttle: {}".format(steer, coll, control.steer, control.throttle))
+
         return control
 
 
